63ef72f53d980b327dd399e8cc1c3eefdfe7fca8
Klajdi Gjonaj <klajdi.gjonaj@gmail.com>
2020-08-09 15:07:00 -0400

    Introduced the wheel.

--- nomic_helper.py
+++ nomic_helper.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import numpy as np
+import time
 
 bot = commands.Bot(command_prefix = '!', description = "Nomic Helper Commands")
 
@@ -24,9 +25,39 @@ async def hello(ctx):
 async def rand(ctx, lower, upper):
     """ Generates a random integer between lower and upper, inclusive"""
     try:
-        response = "Generating a random number between " + str(lower) + " and " + str(upper) + ". RNGesus declares " + str(np.random.randint(lower, upper))
+        number = np.random.randint(lower, upper)
+        await ctx.send("Generating a random number between " + str(lower) + " and " + str(upper) + "...")
+        time.sleep(3)
+        await ctx.send("RNGesus declares " + str(np.random.randint(lower, upper)) + ".")
     except Exception:
-        response = "Arguments must be integers."
+        await ctx.send("Arguments must be integers, with the first integer lower than the next.")
+
+@bot.command()
+async def spin(ctx):
+    """Spins the wheel!"""
+    x = np.random.uniform(0, 1)
+    print("x = ", x)
+    if x <= .40:
+        response = "You gain 1 point!"
+    elif x <= .60:
+        response = "You lose 1 point :("
+    elif x <= .80:
+        response = "You gain 5 points!"
+    elif x <= .85:
+        response = "You lose 5 points :("
+    elif x <= .90:
+        response = "Unlucky. You lose a turn."
+    elif x <= 1:
+        response = "Wow! You gain 10 points!"
+    await ctx.send("You spin the wheel...")
+    time.sleep(3)
+    await ctx.send("It spins around...")
+    time.sleep(3)
+    await ctx.send("It finally starts slowing down...")
+    time.sleep(3)
+    await ctx.send("You look at the result...")
+    time.sleep(3)
     await ctx.send(response)
+
 
 bot.run(token)
